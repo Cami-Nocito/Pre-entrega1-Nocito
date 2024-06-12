1a76d03c6776b4a8720a4aef7c82362234d50697
Cami Nocito <nocito@gmail.com>
2024-06-12 19:48:16 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,38 +1,64 @@
-// Pedido de ingresar un nombre
-let nombreUsuario = prompt("Ingresar tu nombre")
-if (nombreUsuario == "") {
-    alert("No ingresaste un nombre");
-    prompt("Ingresar nombre de usuario")
+//Pedido de nombre nombre
+
+let nombre = prompt("Ingresa tu nombre")
+const persona1 = { nombre }
+if (nombre == "") {
+    alert("No has ingresado un nombre");
+    prompt("Ingresa un nombre para continuar")
 }
 else {
-    alert("Tu nombre es " + nombreUsuario)
+    alert("Bienvenido " + nombre)
+    console.log("Su nombre es" + nombre)
 }
 
-// Cálculo de edad
+
+//Calcular edad para ingresar
+
 const Anio1 = 2024
-const MayoriaEdad = 18
+const MayorDeEdad = 18
 let Anio2 = parseFloat(prompt("Por favor, introduzca su año de nacimiento"));
 function restar(a, b) {
     return a - b;
 }
-let resultado = restar(Anio1, Anio2);
-console.log("Tu edad es:" + resultado);
-alert("Tu edad es: " + resultado);
-if (resultado < MayoriaEdad) {
+
+let resultado = restar(Anio1, Anio2)
+alert("Tienes " + resultado + " años")
+console.log("Su edad es: " + resultado)
+
+if (resultado < MayorDeEdad) {
     alert("Perdón, necesitas tener la mayoría de edad para continuar")
-    let= nuevaSolicitud= prompt("Ingresa tu año de nacimiento");
+    let = nuevaSolicitud = prompt("Ingresa tu año de nacimiento");
 }
-else{
+else {
     alert("Puedes continuar")
+    console.log("Tiene la mayoría de edad")
 }
 
-//Pedido de préstamos
-let Prestamo= prompt("Cuál es el monto que desea pedir?")
-if (Prestamo ==""){
-    alert("no ha ingresado un monto")
-    prompt("ingrese un monto")
+//Comprar productos
+let Harina = 750;
+let MapleDeHuevo = 2500;
+let Azucar = 450;
+let Aceite = 750;
+
+const products = [
+    { Marca: "Harina", precio: 750 },
+    { Marca: "Maple de Huevos", precio: 2500 },
+    { Marca: "Azúcar", precio: 450 },
+    { Marca: "Aceite", precio: 750 }
+];
+
+
+
+
+
+
+
+
+
+
+let ListaDeCompra = prompt("Qué productos desea comprar?: Harina, Maple de Huevos, Azúcar Aceite")
+alert("Usted ha comprado " + ListaDeCompra)
+function sumar(a, b) {
+    return a + b
 }
-else[
-    alert("Su monto es:" + Prestamo)
-]
-console.log("Su prestamo es " + Prestamo)
+let Compra = sumar()
